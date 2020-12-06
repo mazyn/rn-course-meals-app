7b410c7d581e31f7355fb564727d292292db4e55
Gilmar <gilmar@giganet.psi.br>
2020-12-06 16:38:19 -0300

    initial navigation structure

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    padauk: require('./assets/fonts/Padauk-Regular.ttf'),
+    'padauk-bold': require('./assets/fonts/Padauk-Bold.ttf'),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
+  return <MealsNavigator />;
+}
